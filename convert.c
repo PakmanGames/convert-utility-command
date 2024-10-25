cc9a095a972fabe0a5b05c03b80e729b00adc279
Andy Pak <im.andy.pak@gmail.com>
2024-10-24 23:21:27 -0400

    Initial commit
    Create convert.c and Makefile, add pseudo code and planning
    Add initial test files and script

---
+++ convert.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+// TODO Include 2 more .c files with .h
+
+// TODO Global constants for max and min base and other values
+
+// TODO Function to validate input
+
+// TODO Function to read command line flags
+
+// TODO Recursively changing base with convert function
+
+int main() {
+    // TODO read flags function and store values to var based on flags using pointers
+    return EXIT_SUCCESS;
+}
