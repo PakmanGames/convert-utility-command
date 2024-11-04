3eb48da6bcccdf6fad14251e6de70dfd8a57f9e6
Andy Pak <im.andy.pak@gmail.com>
2024-11-04 18:29:58 -0500

    Read from stdin and check if it's a long int

--- convert.c
+++ convert.c
@@ -1,12 +1,14 @@
+/**
+ * convert function that converts numbers into specified base (16 by default),
+ * takes input from stdin or goes through a specified range
+ * Andy Pak, McMaster University, 2024
+ * Additional sources used: manual page for scanf()
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "check_inputs.h"
 #define DEFAULT_BASE 16
-
-// TODO Include 2 more .c files with .h
-
-// TODO Global constants for max and min base and other values
 
 // TODO Function to read command line flags
 
@@ -16,14 +18,21 @@ int main(int argc, char *argv[]) {
     int base = DEFAULT_BASE;
     long start = 0;
     long finish = 0;
-    // TODO read flags function and store values to var based on flags using pointers
-
     if (check_user_inputs(argc, argv, &base, &start, &finish) != 0) {
         return EXIT_FAILURE;
     }
 
-    // for testing
-    printf("YOUR BASE IS %d\n", base);
-    
+    long input;
+    int result;
+    while ((result = scanf("%ld", &input)) != EOF) {
+        if (result == 1) {
+            // FOR TESTING
+            printf("YOu entered %ld\n", input);
+        } else if (result == 0) {
+            printf("Error: Non-long-int token encountered.\n");
+            return EXIT_FAILURE;
+        }
+    }
+
     return EXIT_SUCCESS;
 }
