fd746461dd30b686d9ce1b978f437ddd799d78f4
Andy Pak <im.andy.pak@gmail.com>
2024-11-04 19:17:00 -0500

    Create convert function

--- convert.c
+++ convert.c
@@ -14,6 +14,9 @@
 
 // TODO Recursively changing base with convert function
 
+void convert_by_recursion(long input, int base);
+void convert_by_range(long input, int base, int start, int finish);
+
 int main(int argc, char *argv[]) {
     int base = DEFAULT_BASE;
     long start = 0;
@@ -26,8 +29,16 @@ int main(int argc, char *argv[]) {
     int result;
     while ((result = scanf("%ld", &input)) != EOF) {
         if (result == 1) {
-            // FOR TESTING
-            printf("YOu entered %ld\n", input);
+            if (start == 0 && finish == 0) {
+                if (input == 0) {
+                    printf("0\n");
+                } else {
+                    convert_by_recursion(input, base);
+                    printf("\n");
+                }
+            } else {
+                convert_by_range(input, base, start, finish);
+            }
         } else if (result == 0) {
             printf("Error: Non-long-int token encountered.\n");
             return EXIT_FAILURE;
@@ -35,4 +46,21 @@ int main(int argc, char *argv[]) {
     }
 
     return EXIT_SUCCESS;
+}
+
+void convert_by_recursion(long input, int base) {
+    if (input == 0) {
+        return;
+    } else if (input < 0) {
+        printf("-");
+        input *= -1;
+    }
+
+    convert_by_recursion(input / base, base);
+    const char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%c", digits[input % base]);
+}
+
+void convert_by_range(long input, int base, int start, int finish) {
+    printf("not done\n");
 }
